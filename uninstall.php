556529c3fe8324160e1d91b0d8a0c47fae0b92a2
Ben Upham <bcupham@gmail.com>
2022-03-09 07:18:45 -0800

    uninstall deletes all sisa_ options

--- uninstall.php
+++ uninstall.php
@@ -5,5 +5,15 @@ if (!defined('WP_UNINSTALL_PLUGIN')) {
   die;
 }
 
-delete_option('sisa_api_key');
-delete_option('sisa_pro_api_key');
+// delete_option('sisa_api_key');
+// delete_option('sisa_pro_api_key');
+// delete_option('sisa_pro_api_key');
+
+global $wpdb;
+
+$wpdb->query(
+  "
+  DELETE FROM $wpdb->options
+  WHERE option_name LIKE 'sisa_%'
+"
+);
